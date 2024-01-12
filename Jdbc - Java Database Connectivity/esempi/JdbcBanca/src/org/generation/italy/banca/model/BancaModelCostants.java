3671fd516ba6899e7d024f06e96e6d56e84c321b
pasquarelligeneration <154932365+pasquarelligeneration@users.noreply.github.com>
2024-01-12 20:21:28 +0100

    Add files via upload

---
+++ Jdbc - Java Database Connectivity/esempi/JdbcBanca/src/org/generation/italy/banca/model/BancaModelCostants.java
@@ -0,0 +1,19 @@
+package org.generation.italy.banca.model;
+
+
+
+/**
+ * @author Angelo Pasquarelli
+ * 
+ * In questa classe vengono mantenute le costanti applicative 
+ * utili ai fini del funzionamento del componente model
+ * 
+ */
+
+public class BancaModelCostants {
+
+	public static final Integer maxMovimentiSuConto = 2;
+	
+	public static final Integer maxContiDiProprietaPerUnCliente = 4;
+	
+}

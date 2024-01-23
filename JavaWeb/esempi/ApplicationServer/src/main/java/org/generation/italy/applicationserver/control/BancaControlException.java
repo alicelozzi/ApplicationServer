edb30a6fe52587725d88ea46081d089cc3e21a83
pasquarelligeneration <154932365+pasquarelligeneration@users.noreply.github.com>
2024-01-23 10:31:15 +0100

    Add files via upload

---
+++ JavaWeb/esempi/ApplicationServer/src/main/java/org/generation/italy/applicationserver/control/BancaControlException.java
@@ -0,0 +1,10 @@
+package org.generation.italy.applicationserver.control;
+
+public class BancaControlException extends Exception {
+
+	public BancaControlException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
